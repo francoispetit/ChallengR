c2336a9c5c715afa501b8f6f73a5577d5b67a83a
Alexandre <alex.yam@outlook.fr>
2018-03-12 19:49:46 +0100

    add challenges controller

---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,45 @@
+class ChallengesController < ApplicationController
+ def index
+    @challenges = Challenge.all
+  end
+
+  def new
+    @challenge = Challenge.new
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def edit
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def update
+    @challenge = Challenge.find(params[:id])
+    @challenge.update(challenge_params)
+    redirect_to @challenge
+  end
+
+  def create
+    @challenge = Challenge.create(challenge_params)
+    redirect_to @challenge
+  end
+
+  def destroy
+    @challenge = Challenge.find(params[:id])
+
+    if current_user.id == @challenge.current_user_id
+     @challenge.destroy
+     redirect_to root_path
+    else
+      flash[:danger] = "Désolé, ce challenge n'est pas le votre !"
+    end
+
+  end
+
+  private
+  def challenge_params
+  params.require(:challenge).permit(:goal, :duedate)
+  end
+end
