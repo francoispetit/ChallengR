354a4d3948e3e110cfc6aaff6eab33e9e1be17fb
Alikae <alikae@laposte.net>
2018-03-14 16:45:06 +0100

    add spec model category participation

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(username:"testname", email:"test@email.com", password:"azerty")
+    @chall = Challenge.new(goal:"hihi")
+    @chall2 = Challenge.new(goal:"haha")
+    @user.organized_challenges << @chall
+    @user.organized_challenges << @chall2
+    @chall.save
+    @chall2.save
+    @ca = Category.create
+    @ca2 = Category.create
+  end
+
+  it "can have multiple challenges" do
+    @ca.challenges << @chall
+    @ca.challenges << @chall2
+    expect(@ca.challenges).to eq([@chall, @chall2])
+  end
+
+  it "can be multiple on challenges" do
+    @ca.challenges << @chall
+    @ca2.challenges << @chall
+    expect(@chall.categories).to eq([@ca, @ca2])
+  end
+
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+
+  end
+
+  it "save with valid data" do
+
+  end
+
+  it "don't save with invalid data" do
+
+  end
+
+  it "can be on challenge" do
+
+  end
+
+  it "can be on comment" do
+
+  end
 end
