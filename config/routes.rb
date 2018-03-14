658853c85cc01bbdae2fa7c0ddd3a56fbd2d9a80
francoispetit <francois.petit@outlook.com>
2018-03-14 12:30:30 +0100

    add model & asso comments + modif view challenge#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
 
   	root "challenges#index"
 
+	post "challenge/join_challenge/:id", to: "challenges#join_challenge", as: "join_challenge"
+
 	devise_for :users
-	resources :challenges
+	resources :challenges do
+		resources  :comments
+	end
+	resources  :comments do
+		resources  :comments
+	end
+
 	resources :subgoals
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
