ad1fdf5c78a62edca0b18a9e57b840d95732a423
Alikae <alikae@laposte.net>
2018-03-15 16:32:44 +0100

    correct seeds + add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314175014) do
+ActiveRecord::Schema.define(version: 20180315113649) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20180314175014) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
