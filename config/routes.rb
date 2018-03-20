e4d56ef509437fae4eebec76de0d34881712cf09
Alikae <alikae@laposte.net>
2018-03-20 13:33:45 +0100

    QSDF

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   get 'categories/create'
 
   get 'categories/update'
@@ -9,10 +8,6 @@ Rails.application.routes.draw do
 
   	root "challenges#index"
 		get 'challenges/redindex' => 'challenges#red_index'
-=======
-	root "challenges#index"
-	get 'challenges/redindex' => 'challenges#red_index'
->>>>>>> 41e602704de6bd7fbbe44e2eed42b61182da48a8
 
 	post "challenge/join_challenge/:id", to: "challenges#join_challenge", as: "join_challenge"
 
