f27b961f5ac90f9370c677a849078c4c7374e6fa
Alikae <alikae@laposte.net>
2018-03-22 17:17:42 +0100

    .

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -90,7 +90,7 @@
   def remove_category()
     @challenge = Challenge.find(params[:id])
     @challenge.categories.find(params[:catid]).delete if @challenge.categories.include? Category.find(params[:catid])
-    #redirect_to challenge_path(@challenge)
+    redirect_to challenge_path(@challenge)
   end
 
 
