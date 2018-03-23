aa7c0d17b5d7ad0d0b17b63af4b919864e842ee7
Alexandre <alex.yam@outlook.fr>
2018-03-23 02:14:28 +0100

    messaging without ajax works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318210139) do
+ActiveRecord::Schema.define(version: 20180322235241) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -60,6 +60,10 @@ ActiveRecord::Schema.define(version: 20180318210139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "conversation_id"
+    t.text "body"
+    t.integer "user_id"
+    t.boolean "read", default: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "participations", force: :cascade do |t|
